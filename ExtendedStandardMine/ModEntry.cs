a06d8d8e289e02773efc675c1695b3d4a53aa0f1
Justus Schneider <justusschneider91@gmail.com>
2025-04-20 00:20:46 +0200

    Add tile replacement in code and make new ladder non passable

--- ExtendedStandardMine/ModEntry.cs
+++ ExtendedStandardMine/ModEntry.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
+using xTile;
+using xTile.Layers;
+using xTile.Tiles;
+using xTile.Dimensions;
 
 namespace ExtendedStandardMine
 {
@@ -17,10 +21,48 @@ namespace ExtendedStandardMine
         {
             I18n.Init(helper.Translation);
             helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+            helper.Events.Content.AssetRequested += this.ReplaceLevel120;
             this.Monitor.Log($"ExtendedStandardMine loaded successfully. :)", LogLevel.Info);
         }
 
-        private void OnGameLaunched(object sender, EventArgs e)
+        private void ReplaceLevel120(object? sender, AssetRequestedEventArgs e)
+        {
+            if (e.Name.IsEquivalentTo("Maps/Mines/120"))
+            {
+                e.Edit(asset =>
+                {
+                    IAssetDataForMap editor = asset.AsMap();
+                    Map map = editor.Data;
+
+                    Layer layer = map.GetLayer("Back");
+
+                    layer.Tiles[14, 11] = new StaticTile(
+                        layer: layer,
+                        tileSheet: map.GetTileSheet("mine"),
+                        tileIndex: 173,
+                        blendMode: BlendMode.Alpha
+                    );
+
+                    Tile tile = this.GetTile(map, layer, 14, 11);
+                    if (tile == null)
+                    {
+                        return;
+                    }
+                    tile.Properties["Passable"] = "F";
+                    
+                });
+            }
+            return;
+        }
+
+        private Tile GetTile(Map map, Layer layer, int tileX, int tileY)
+        {
+            Location pixelPosition = new Location(tileX * Game1.tileSize, tileY * Game1.tileSize);
+
+            return layer.PickTile(pixelPosition, Game1.viewport.Size);
+        }
+
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
         {
             return;
         }
