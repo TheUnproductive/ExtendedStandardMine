d285c94117662ea64e2eea448c70deea749ab0d7
Justus Schneider <justusschneider91@gmail.com>
2025-04-19 23:26:16 +0200

    Projektdateien hinzufügen.

---
+++ ExtendedStandardMine/ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace ExtendedStandardMine
+{
+    internal sealed class ModEntry : Mod
+    {
+
+        private ModConfig _config;
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+            this.Monitor.Log($"ExtendedStandard Mine loaded successfully. :)", LogLevel.Info);
+        }
+
+        private void OnGameLaunched(object sender, EventArgs e)
+        {
+            return;
+        }
+    }
+}
